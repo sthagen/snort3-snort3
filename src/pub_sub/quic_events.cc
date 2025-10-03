4bf8312aacd4336b33edd2548d04328f772889ee
Brian Morris (bmorris2) <bmorris2@cisco.com>
2025-10-03 15:58:05 +0000

    Pull request #4908: quic advanced logging
    Merge in SNORT/snort3 from ~BMORRIS2/snort3:quic_events to master
    
    Squashed commit of the following:
    
    commit 92a10ddfbb99ddeff8e13c96c8ffab6bf9c995ea
    Author: Brian Morris <bmorris2@cisco.com>
    Date:   Tue Sep 30 11:12:06 2025 -0500
    
        pub_sub: add quic logging events

---
+++ src/pub_sub/quic_events.cc
@@ -0,0 +1,28 @@
+//--------------------------------------------------------------------------
+// Copyright (C) 2025-2025 Cisco and/or its affiliates. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License Version 2 as published
+// by the Free Software Foundation.  You may not use, modify or distribute
+// this program under any other version of the GNU General Public License.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//--------------------------------------------------------------------------
+// quic_events.cc author Brian Morris <bmorris2@cisco.com>
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "quic_events.h"
+
+using namespace snort;
+
+const PubKey quic_logging_pub_key { "quic", QuicLoggingEventIds::QUIC_MAX_EVENT };
